3f47f2115b8ed53367cf0c8d2dd08af42e42c9d3
jeankvd <djjean85@gmail.com>
2018-01-28 20:58:04 -0500

    added structure and styles for top bars

--- functions.php
+++ functions.php
@@ -119,8 +119,12 @@ add_action( 'widgets_init', 'theme_boilerplate_widgets_init' );
 function theme_boilerplate_scripts() {
 	wp_enqueue_style( 'theme-boilerplate-style', get_stylesheet_uri() );
 	wp_enqueue_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css' );	
+	wp_enqueue_style( 'font_awesome', 'https://use.fontawesome.com/abebc5126b.css', '', '', '' );	
+	wp_enqueue_style( 'sass-style', get_template_directory_uri() . "/sass-styles.css" );	
 
+	wp_enqueue_script( "font_awesome-js", 'https://use.fontawesome.com/abebc5126b.js', '', '' , true ); 
 	wp_enqueue_script( 'main-js', get_template_directory_uri() . '/js/app.js', array('jquery'), '20151215', true );
+	
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
--- header.php
+++ header.php
@@ -21,18 +21,57 @@
 </head>
 
 <body <?php body_class(); ?>>
+
+<!-- Top Bar -->
+<div id="top-bar" class="container-fluid row">
+	<div class="col-3">
+		Welcome to Proffesional Roofing Inc.
+	</div>
+	<div class="col-9">
+		clock Monday-Friday : 8:00 AM to 7:00 PM
+	</div>
+</div>
+<!-- Top Info -->
+<div id="top-info" class=" container-fluid row">
+	<div class="col-3">
+			logo
+	</div>
+		<div class="col-3 info-card">
+			<div class="col-2"><i class="fa fa-home" aria-hidden="true"></i></div>
+			<div class="col-9">
+				<h4>321, Breaking Street</h4>
+					<p>Newyork, USA 10002</p>
+			</div>
+			</div>
+			<div class="col-3 info-card">
+			<div class="col-2"><i class="fa fa-phone" aria-hidden="true"></i></div>
+			<div class="col-9">
+				<h4>Call Us On</h4>
+					<p>1-800-232-3293</p>
+			</div>
+			</div>
+			<div class="col-3 info-card">
+			<div class="col-2"><i class="fa fa-envelope" aria-hidden="true"></i></div>
+			<div class="col-9">
+				<h4>Mail Us @</h4>
+				<p>SupportUs@gmail.com</p>
+				</div>	
+			</div>		
+</div>
+
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'theme-boilerplate' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<nav id="site-navigation" class="main-navigation navbar navbar-dark bg-dark">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'theme-boilerplate' ); ?></button>
+		<nav id="site-navigation" class="main-navigation navbar">
+			<!-- <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'theme-boilerplate' ); ?></button> -->
 			<?php
 				wp_nav_menu( array(
 					'theme_location' => 'menu-1',
 					'menu_id'        => 'primary-menu',
 				) );
 			?>
+			<a href="" class="btn btn-primary">Contact Us</a>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
