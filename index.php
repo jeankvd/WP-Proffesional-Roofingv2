8cf6a5fc7c35f61536c557c3159f436e14ca37c6
jeankvd <djjean85@gmail.com>
2018-02-04 21:53:06 -0500

    added hero

--- index.php
+++ index.php
@@ -18,6 +18,19 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
+		<div class="jumbotron jumbotron-fluid" id="">
+			<div class="container">
+				<h1 class="display-3">Fluid jumbotron</h1>
+				<p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+			</div>
+		</div>
+
+		<div class="container"  id="about-us">
+			<div class="row">
+				<div class=""></div>
+			</div>
+		</div>
+
 
 		<?php
 		if ( have_posts() ) :
