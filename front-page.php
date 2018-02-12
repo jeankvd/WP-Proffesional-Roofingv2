08d9a131456ad737fdd8b077aca2c654e0ebff23
jeankvd <djjean85@gmail.com>
2018-02-11 22:03:43 -0500

    container fixes and ACF integration to home

--- front-page.php
+++ front-page.php
@@ -26,88 +26,106 @@ get_header(); ?>
 		</div>
 
 		<!-- About Us -->
-		<div class="container-fluid"  id="about-us">
-			<div class="row">
-				<div class="col-6">
-                    <img src="<?php the_field('about_us_image'); ?>" alt="" class="img-fluid">
-				</div>
-				<div class="col-6">
-					<h2><?php the_field('about_us_first_title');?>&nbsp;<span><?php the_field('about_us_second_title'); ?></span></h2>
-					<p><?php the_field('about_us_text'); ?></p>
-					<div class="row">
-						<div class="col-4 statistic">icon and info</div>
-						<div class="col-4 statistic">icon and info</div>
-						<div class="col-4 statistic">icon and info</div>			
+		<div id="about-us">
+			<div class="container" >
+				<div class="row">
+					<div class="col-6">
+						<img src="<?php the_field('about_us_image'); ?>" alt="" class="img-fluid">
+					</div>
+					<div class="col-6">
+						<h2><?php the_field('about_us_first_title');?>&nbsp;<span><?php the_field('about_us_second_title'); ?></span></h2>
+						<p><?php the_field('about_us_text'); ?></p>
+						<div class="row">
+							<div class="col-4 statistic">
+								<span class="icon-holder"><i class="fa fa-user" aria-hidden="true"></i></span>
+								<span><?php the_field('workers');?></span>
+								<span>Engineers & Workers</span>
+							</div>
+							<div class="col-4 statistic">
+								<span class="icon-holder"><i class="fa fa-building" aria-hidden="true"></i></span>
+								<span><?php the_field('offices');?></span>
+								<span>Offices</span>
+							</div>
+							<div class="col-4 statistic">
+								<span class="icon-holder"><i class="fa fa-gavel" aria-hidden="true"></i></span>
+								<span><?php the_field('projects');?></span>
+								<span>Projects Completed</span>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 
 		<!-- Our Services -->
-		<div class="container-fluid" id="our-services">
-			<div class="row">
-				<div class="col-4">
-					<h2 class="title-underline"><?php the_field('services_title'); ?></h2>
+		<div id="our-services">
+			<div class="container" >
+				<div class="row">
+					<div class="col-4">
+						<h2 class="title-underline"><?php the_field('services_title'); ?></h2>
+					</div>
+					<div class="col-8">
+						<p><?php the_field('services_text'); ?></p>
+					</div>
 				</div>
-				<div class="col-8">
-					<p><?php the_field('services_text'); ?></p>
+				<div class="row">
+					<?php $loop = new WP_Query(
+						array(
+							'post_type' => 'Services'
+						)
+						); ?>
+						<?php
+						$i = 0;
+						if ( $loop->have_posts() ) : while ( $loop->have_posts() && $i < 6 ) : $loop->the_post(); ?>
+
+						<div class="col-4">
+							<img src="<?php the_post_thumbnail_url(); ?>" alt="" class="img-fluid">
+							<h4><?php the_title(); ?></h4>
+							<p><?php the_field('excerpt'); ?></p>
+						</div>
+
+						<?php $i++; ?>
+						<?php endwhile; ?>
+
+						<?php else: ?>
+							<h3>No Services Were Found</h3>
+						<?php endif; ?>
+						<?php wp_reset_postdata(); ?> 
 				</div>
-			</div>
-			<div class="row">
-                <?php $loop = new WP_Query(
-                    array(
-                        'post_type' => 'Services'
-                    )
-                    ); ?>
-					<?php
-					$i = 0;
-					if ( $loop->have_posts() ) : while ( $loop->have_posts() && $i < 6 ) : $loop->the_post(); ?>
-
-                    <div class="col-4">
-                        <img src="<?php the_post_thumbnail_url(); ?>" alt="" class="img-fluid">
-						<h4><?php the_title(); ?></h4>
-						<p><?php the_field('excerpt'); ?></p>
-                    </div>
-
-					<?php $i++; ?>
-                    <?php endwhile; ?>
-
-                    <?php else: ?>
-                        <h3>No Services Were Found</h3>
-                    <?php endif; ?>
-                    <?php wp_reset_postdata(); ?> 
 			</div>
 		</div>
 
 		<!-- Latest Projects -->
-		<div class="container-fluid" id="latest-projects">
-			<h2 class="title-underline"><?php the_field('projects_title'); ?></h2>
-			<div class="owl-carousel">
-			<?php $loop = new WP_Query(
-                    array(
-                        'post_type' => 'Projects'
-                    )
-                    ); ?>
-					<?php if ( $loop->have_posts() ) : while ( $loop->have_posts() ) : $loop->the_post(); ?>
+		<div id="latest-projects">
+			<div class="container">
+				<h2 class="title-underline"><?php the_field('projects_title'); ?></h2>
+				<div class="owl-carousel">
+				<?php $loop = new WP_Query(
+						array(
+							'post_type' => 'Projects'
+						)
+						); ?>
+						<?php if ( $loop->have_posts() ) : while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
-                    <div class="project">
-                        <img src="<?php the_post_thumbnail_url(); ?>" alt="" class="img-fluid">
+						<div class="project">
+							<img src="<?php the_post_thumbnail_url(); ?>" alt="" class="img-fluid">
+							<div class="project-overlay">
+							</div>
+							<h6><?php the_title(); ?></h6>
+							<span>Latest Projects</span>
+						</div>
+						<?php endwhile; ?>
+
+						<?php else: ?>
+							<h3>No Projects Were Found</h3>
+						<?php endif; ?>
+					<?php wp_reset_postdata(); ?> 
+					<div class="project">
+						<img src="http://tonatheme.com/newwp/factory/wp-content/uploads/2017/07/p2.jpg" alt="" class="img-fluid">
 						<div class="project-overlay">
 						</div>
-						<h6><?php the_title(); ?></h6>
-						<span>Latest Projects</span>
-                    </div>
-                    <?php endwhile; ?>
-
-                    <?php else: ?>
-                        <h3>No Projects Were Found</h3>
-                    <?php endif; ?>
-                <?php wp_reset_postdata(); ?> 
-				<div class="project">
-					<img src="http://tonatheme.com/newwp/factory/wp-content/uploads/2017/07/p2.jpg" alt="" class="img-fluid">
-					<div class="project-overlay">
+						<h6>Multi String Project</h6>
 					</div>
-					<h6>Multi String Project</h6>
 				</div>
 			</div>
 		</div>
@@ -115,44 +133,46 @@ get_header(); ?>
 
 
 		<!-- Blog -->
+		<div id="blog">
+			<div class="container" >
+					<div class="row">
+			<?php
+			$temp = $wp_query; $wp_query= null;
+			$wp_query = new WP_Query(); $wp_query->query('posts_per_page=3');
+			while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
 
-		<div class="container" id="blog">
-				<div class="row">
-		<?php
-		$temp = $wp_query; $wp_query= null;
-		$wp_query = new WP_Query(); $wp_query->query('posts_per_page=3');
-		while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
+			<div class="col-4">
+				<img src="<?php the_post_thumbnail_url(); ?>" alt="" class="img-fluid">
+				<h6><?php the_title();?></h6>
+				<span>by <?php the_author();?> | <?php the_date('F j');?></span>
+				<p><?php the_excerpt(); ?></p>
+			</div>
 
-		<div class="col-4">
-			<img src="<?php the_post_thumbnail_url(); ?>" alt="" class="img-fluid">
-			<h6><?php the_title();?></h6>
-			<span>by <?php the_author();?> | <?php the_date('F j');?></span>
-			<p><?php the_excerpt(); ?></p>
-		</div>
-
-		<?php endwhile; ?>
-		<?php wp_reset_query(); ?>
+			<?php endwhile; ?>
+			<?php wp_reset_query(); ?>
+			</div>
 		</div>
 	</div>
 
 	<!-- Contact Us -->
-
-	<div class="container-fluid" id="contact-us">
-		<div class="row">
-			<div class="col-5">
-				<h2 class="title-underline"><?php the_field('contact_us_title'); ?></h2>
-				<p><?php the_field('contact_us_text'); ?>
-				</p>
-			</div>
-			<div class="col-7">
-				<form action="" class="row">
-					<input type="name" name="name" id="name" class="col-5" placeholder="Your Name*">
-					<input type="email" name="email" id="email" class="col-5" placeholder="Your E-Mail*">
-					<input type="tel" name="tel" id="tel" class="col-5" placeholder="Phone">
-					<input type="text" name="subject" id="subject" class="col-5" placeholder="Subject">
-					<textarea class="col-11" name="message" id="message" placeholder="Your Message"></textarea>
-					<input type="button" value="Get In Contact">
-				</form>
+	<div id="contact-us">
+		<div class="container" >
+			<div class="row">
+				<div class="col-5">
+					<h2 class="title-underline"><?php the_field('contact_us_title'); ?></h2>
+					<p><?php the_field('contact_us_text'); ?>
+					</p>
+				</div>
+				<div class="col-7">
+					<form action="" class="row">
+						<input type="name" name="name" id="name" class="col-5" placeholder="Your Name*">
+						<input type="email" name="email" id="email" class="col-5" placeholder="Your E-Mail*">
+						<input type="tel" name="tel" id="tel" class="col-5" placeholder="Phone">
+						<input type="text" name="subject" id="subject" class="col-5" placeholder="Subject">
+						<textarea class="col-11" name="message" id="message" placeholder="Your Message"></textarea>
+						<input type="button" value="Get In Contact">
+					</form>
+				</div>
 			</div>
 		</div>
 	</div>
